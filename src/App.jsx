3f2a590cd1097a4aae8313466c7d531e01537389
Higoralmeida702 <higoralmeida7022@gmail.com>
2024-04-03 13:56:31 -0300

    Adição de navegacao multiPages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
+import React from 'react';
+import './styles.css';
+import { BrowserRouter, Routes, Link, Route } from 'react-router-dom';
+import Inicio from './componentes/inicio';
+
+
 function App() {
- 
   return (
     <div>
-      h1dsakdjsalkdj
+      <header className='headerNavegacao'>
+
+      <BrowserRouter>
+      <img src="./images/logo3.png" alt="" />
+        <ul>
+          <li className='menu-item'><Link to="/inicio">Inicio</Link></li>
+          <li className='menu-item'><Link to="/cursos">Cursos</Link></li>
+          <li className='menu-item'><Link to="/vagas">Vagas</Link></li>
+          <li className='menu-item'><Link to="/noticias">Noticías</Link></li>
+          <li className='menu-item'><Link to="/contato">Contato</Link></li>
+        </ul>
+        <img className="menu-icones" src="./images/FacebookLogo.png" alt="" />
+        <img className="menu-icones" src="./images/InstagramLogo.png" alt="" />
+        <img className="menu-icones" src="./images/LinkedinLogo.png" alt="" />
+        <Routes>
+          <Route path="/" element={<Inicio/>} />      
+        </Routes>
+      </BrowserRouter>
+      </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
