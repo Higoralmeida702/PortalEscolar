bd58943f341bfd0dc103429ff1daed022b9c3136
Higoralmeida702 <higoralmeida7022@gmail.com>
2024-04-04 19:25:50 -0300

    Finalizada pagina cursos

---
+++ src/componentes/cursos/cursos.jsx
@@ -0,0 +1,11 @@
+import SubHeaderCursos from "./subHeaderCursos"
+
+const Cursos = () => {
+    return (
+        <div>
+          <SubHeaderCursos/>
+        </div>
+    )
+}
+
+export default Cursos
---
+++ src/componentes/cursos/subHeaderCursos/index.jsx
@@ -0,0 +1,42 @@
+import './desenvolvimentoDeSistema.css'
+
+const DesenvolvimentoDeSistema = () => {
+    return (
+        <div>
+            <div className='desenvolvimentoSubHeader'>desenvolvimento de sistema</div>
+            <section className='mainDesenvolvimentoDeSistema'>
+               <aside><img src="./images/Rectangle224.png" alt=""/></aside>
+                    <div className="conteudoPrincipalSistema">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet vitae vestibulum iaculis lacus aliquam convallis dolor risus. Velit vitae in lectus facilisis arcu tempor. Tempus dolor convallis accumsan nulla mi amet. Vulputate risus neque tellus faucibus molestie sem mauris. Semper quis orci scelerisque lectus. Leo sollicitudin cursus diam maecenas posuere at malesuada maecenas. Facilisis nunc nunc pellentesque libero arcu et leo dignissim. Pretium nulla leo pharetra, fermentum magnis. Odio molestie nec, vulputate tellus tellus.</p>
+                    <div className='div2'>
+                    <img src="./images/Sun.png" alt="" />
+                        <h3>Período:</h3>
+                        <p>Noturno</p>
+                        </div>
+                        <div className='div2'>
+                 <img src="./images/User.png" alt="" />
+                        <h3>Coordenação:</h3>
+                        <p>Luri Silva</p>
+                        </div>
+                        <div className='div2'>
+                    <img src="./images/HourglassMedium.png" alt="" />
+                        <h3>Duração:</h3>
+                        <p>2 anos</p>
+                        </div>
+                    </div>
+            </section>
+            <div className="descricao descricaoFundoCinza">
+              <div className='descricaoConteudo'> 
+                <h2 className='descricaoTitulo'>Objetivos gerais</h2>
+                <p className='descricaoTexto'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet vitae vestibulum iaculis lacus aliquam convallis dolor risus. Velit vitae in lectus facilisis arcu tempor. Tempus dolor convallis accumsan nulla mi amet. Vulputate risus neque tellus faucibus molestie sem mauris. Semper quis orci scelerisque lectus. Leo sollicitudin cursus diam maecenas posuere at malesuada maecenas. Facilisis nunc nunc pellentesque libero arcu et leo dignissim. Pretium nulla leo pharetra, fermentum magnis. Odio molestie nec, vulputate tellus tellus.</p>
+            </div>
+            </div>
+             <div className='descricaoConteudo'> 
+                <h2 className='descricaoTitulo'>Objetivos gerais</h2>
+                <p className='descricaoTexto'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet vitae vestibulum iaculis lacus aliquam convallis dolor risus. Velit vitae in lectus facilisis arcu tempor. Tempus dolor convallis accumsan nulla mi amet. Vulputate risus neque tellus faucibus molestie sem mauris. Semper quis orci scelerisque lectus. Leo sollicitudin cursus diam maecenas posuere at malesuada maecenas. Facilisis nunc nunc pellentesque libero arcu et leo dignissim. Pretium nulla leo pharetra, fermentum magnis. Odio molestie nec, vulputate tellus tellus.</p>
+             </div>
+        </div>
+    )
+}
+
+export default DesenvolvimentoDeSistema
